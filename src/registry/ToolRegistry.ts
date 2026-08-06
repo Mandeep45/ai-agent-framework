2791046dab7b3f294e6a13ccf67e2bf77eb2c791
Mandeep Singh <mandeepdhariwal2001@gmail.com>
2026-08-06 16:39:19 +0530

    feat: implement ToolRegistry

---
+++ src/registry/ToolRegistry.ts
@@ -0,0 +1,18 @@
+import { Tool } from "../types/tools";
+
+export class ToolRegistry {
+  private tools = new Map<string, Tool>();
+
+  register(tool: Tool): void {
+    this.tools.set(tool.name, tool);
+  }
+
+  get(name: string): Tool | undefined {
+    return this.tools.get(name);
+  }
+
+  getAll(): Tool[] {
+    return Array.from(this.tools.values());
+  }
+}
+
